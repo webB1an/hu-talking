ea43801ece3d1b94f4dec28cff96d2c7f9c05df7
webB1an <webB1anyaoyao@gmail.com>
2023-03-09 11:08:41 +0800

    fix

--- routes/chat.ts
+++ routes/chat.ts
@@ -5,6 +5,10 @@ import type { Router, Request, Response } from 'express'
 const router: Router = express.Router()
 
 router.post('/chat', async(req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'text/event-stream')
+  res.setHeader('Cache-Control', 'no-cache')
+  res.setHeader('Connection', 'keep-alive')
+
   const { apiKey, messages } = req.body
 
   console.log('apiKey', apiKey)
@@ -14,18 +18,14 @@ router.post('/chat', async(req: Request, res: Response) => {
     apiKey
   })
 
-  const evtSource = openai.createChatCompletion({
+  const openai = new OpenAIApi(configuration)
+
+  res.write(openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     temperature: 0.9,
     stream: true,
     messages
-  })
-
-  evtSource.onmessage = function(event) {
-    console.log('onmessage', event.data)
-  }
-
-  // const openai = new OpenAIApi(configuration)
+  }))
   // let response
   // try {
   //   response = await openai.createChatCompletion({
