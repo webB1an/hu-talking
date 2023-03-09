4a226f765b4c64e75f0543b2fb0cd547830f04c4
webB1an <webB1anyaoyao@gmail.com>
2023-03-09 09:23:13 +0800

    fix

--- routes/chat.ts
+++ routes/chat.ts
@@ -5,33 +5,27 @@ import type { Router, Request, Response } from 'express'
 const router: Router = express.Router()
 
 router.post('/chat', async(req: Request, res: Response) => {
-  const { apiKey } = req.body
+  const { apiKey, messages } = req.body
 
   console.log('apiKey', apiKey)
+  console.log('messages', messages)
 
   const configuration = new Configuration({
     apiKey
   })
 
   const openai = new OpenAIApi(configuration)
-  // console.log('openai', openai)
   let response
   try {
     response = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
       temperature: 0.9,
       stream: true,
-      messages: [{ 'role': 'user', 'content': '介绍下你自己!' }]
+      messages
     })
   } catch (error) {
     console.log('---------------error---------------', error)
   }
-
-  if (response?.data) {
-    console.log('response', response)
-  }
-
-  console.log(typeof response)
 
   res.json({
     code: 90001,
